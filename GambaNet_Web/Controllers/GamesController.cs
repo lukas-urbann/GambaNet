e0f81a03324052b6f77a950c5d70e450cb0fb776
Lukáš Urban <lukas.urbann@seznam.cz>
2024-12-16 20:24:59 +0100

    mezistav

---
+++ GambaNet_Web/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GambaNet_Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /* --- Nefunguje přetížení
+        public IActionResult Index(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        */
+    }
+}
