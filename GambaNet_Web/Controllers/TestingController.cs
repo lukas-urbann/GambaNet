e30135bcd4c70486e8ef41baee3e0e7442d4f479
Lukáš Urban <lukas.urbann@seznam.cz>
2024-12-23 23:37:46 +0100

    /Testing/Cups

---
+++ GambaNet_Web/Controllers/TestingController.cs
@@ -0,0 +1,15 @@
+﻿using GambaNet_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using GambaNet_Web.Application.Abstraction;
+using GambaNet_Web.Application.ViewModel;
+
+namespace GambaNet_Web.Controllers;
+
+public class TestingController : Controller
+{
+    public IActionResult Cups()
+    {
+        return View();
+    }
+}
