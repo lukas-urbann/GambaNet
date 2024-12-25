d0dbe74251dd1f9616360c0a9aedb35a9f8f9a60
Lukáš Urban <lukas.urbann@seznam.cz>
2024-12-25 01:31:35 +0100

    Create dataloader.php

---
+++ GambaNet_Game/Assets/_PHP/dataloader.php
@@ -0,0 +1,78 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "admin";
+$dbname = "gambanet_web";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error)
+{
+    die("500");
+}
+
+$request_type = $_POST["requestType"];  
+$user_id = $_POST["userId"];  
+$game_id = $_POST["gameId"];  
+$new_value = $_POST["newValue"];  
+
+$rowName = "NULL";
+$sql = "NULL";
+
+switch ($request_type)
+{
+    case "UserBalanceDownload":
+        $sql = "SELECT * FROM aspnetusers WHERE Id = $user_id";
+        $rowName = "Balance";
+    break;
+    case "UserBalanceUpload":
+        $sql = "UPDATE aspnetusers SET Balance = $new_value WHERE Id = $user_id";
+    break;
+    case "GameDataWinrateDownload":
+        $sql = "SELECT * FROM game WHERE Id = $game_id";
+        $rowName = "Winrate";
+    break;
+    case "GameDataNameDownload":
+        $sql = "SELECT * FROM game WHERE Id = $game_id";
+        $rowName = "Name";
+    break;
+    case "GameDataColorRedownload":
+        $sql = "SELECT * FROM game WHERE Id = $game_id";
+        $rowName = "BackgroundRed";
+    break;
+    case "GameDataColorBlueDownload":
+        $sql = "SELECT * FROM game WHERE Id = $game_id";
+        $rowName = "BackgroundBlue";
+    break;
+    case "GameDataColorGreenDownload":
+        $sql = "SELECT * FROM game WHERE Id = $game_id";
+        $rowName = "BackgroundGreen";
+    break;
+    default:
+        echo "Invalid request type";
+        $conn->close();
+        return;
+}
+
+$result = $conn->query($sql);
+
+if ($rowName == "NULL")
+{
+    echo "200";
+    $conn->close();
+    return;
+}
+
+if ($result->num_rows > 0)
+{
+    while($row = $result->fetch_assoc())
+    {
+        echo $row[$rowName];
+    }
+}
+else
+{
+    echo "404";
+}
+
+$conn->close();
