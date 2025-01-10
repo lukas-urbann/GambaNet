830ed20a8e084399aa09eaef5eabe01d25f86d7b
Jakub Kramný <57757403+kyrkCZ@users.noreply.github.com>
2025-01-10 16:48:24 +0100

    Added unit test for home controller

--- GambaNet_Web.Test/UnitTest1.cs
+++ GambaNet_Web.Test/UnitTest1.cs
@@ -1,10 +1,27 @@
+using GambaNet_Web.Application.Abstraction;
+using GambaNet_Web.Application.Implementation;
+using GambaNet_Web.Application.ViewModel;
+using GambaNet_Web.Domain.Entities;
+using Moq;
+
 namespace GambeNet_Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GetIndexViewModel_ReturnsGameViewModelWithGames()
     {
-        Assert.True(true);
+        // Arrange
+        var gameAppService = new Mock<IGameAppService>();
+        gameAppService.Setup(x => x.Select()).Returns(new List<Game>
+        {
+            new Game { Id = 1, Name = "Game 1" },
+            new Game { Id = 2, Name = "Game 2" }
+        });
+        var homeService = new HomeService(gameAppService.Object);
+        // Act
+        GameViewModel gameViewModel = homeService.GetIndexViewModel();
+        // Assert
+        Assert.Equal(2, gameViewModel.Games.Count);
     }
 }
