4e6a228ce765540d1295dddb786e4e117e616f11
Jakub Kramný <57757403+kyrkCZ@users.noreply.github.com>
2025-01-13 20:39:45 +0100

    Validace create edit ger

---
+++ GambaNet_Web/Migrations/20250113193914_AddGameTypes4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GambaNet_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameTypes4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartDate",
+                value: new DateTime(2025, 1, 13, 20, 39, 13, 844, DateTimeKind.Local).AddTicks(7998));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StartDate",
+                value: new DateTime(2025, 1, 13, 20, 39, 13, 844, DateTimeKind.Local).AddTicks(8032));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartDate",
+                value: new DateTime(2025, 1, 13, 0, 15, 3, 165, DateTimeKind.Local).AddTicks(647));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StartDate",
+                value: new DateTime(2025, 1, 13, 0, 15, 3, 165, DateTimeKind.Local).AddTicks(711));
+        }
+    }
+}
